de53236175e3bba659b0cf7a560c31fe1ea6170a
Jakub Kriz <jakubykriz@gmail.com>
2025-06-08 23:17:38 +0200

    Upgrade to Go 1.24 and update development tooling

--- cmd/day.go
+++ cmd/day.go
@@ -328,6 +328,7 @@ func notifyDayStart(shutdownTimeMinutes int) {
 
 func scheduleNotification(t time.Time, content string) {
 	datetime := t.Format("2006-01-02 15:04")
+	// #nosec G204 - datetime is controlled format and content is from internal string constants
 	cmd := exec.Command("systemd-run", "--user", "--on-calendar="+datetime, "notify-send", content)
 	_ = cmd.Run() // ignore errors for now
 }
